343e442e43ef91b97b291f93c65c4c3759a02c95
farikkm <farikk.v.2@gmail.com>
2025-08-10 06:19:18 +0500

    dotenv added to the project

--- server.js
+++ server.js
@@ -1,4 +1,15 @@
+import dotenv from "dotenv";
+
+dotenv.config({ path: "./.env" });
+
+const username = process.env.username;
+const db = process.env.database;
+
+console.log(`Hello, ${username}`);
+
 console.log("Hello Node.js and my favourite helper Warp!");
+
+console.log(`It's my ${db}`);
 
 matrix(1, 0, 0, 0, 1, 0, 0, 0, 1);
 
