41928016e289bc4824a02260fcdb0fee39d92417
farikkm <farikk.v.2@gmail.com>
2025-08-12 19:57:12 +0500

    connection to mongoDB

--- src/server.js
+++ src/server.js
@@ -1,11 +1,19 @@
 import app from "./app.js";
 import dotenv from "dotenv";
+import connectDB from "./db/index.js";
 
 dotenv.config({
 	path: "./.env",
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`);
-});
+
+async function main() {
+	await connectDB();
+
+	app.listen(port, () => {
+		console.log(`Example app listening on port ${port}`);
+	});
+}
+
+main();
