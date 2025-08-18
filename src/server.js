c608fdb61f62524c7a74b6b6b0181b73e8e17cea
farikkm <farikk.v.2@gmail.com>
2025-08-18 14:08:23 +0500

    deleted unnecessary comment

--- src/server.js
+++ src/server.js
@@ -12,12 +12,6 @@ const port = process.env.PORT || 3000;
 async function main() {
 	await connectDB();
 
-	// const new_user = new User({
-	// 	username: "fariz",
-	// 	password: "2134",
-	// 	fullname: "fariz mirzayev",
-	// });
-
 	app.listen(port, () => {
 		console.log(`Example app listening on port ${port}`);
 	});
